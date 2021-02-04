52eed67e5bd49221dc0a94f6925163aa7e6c7c03
Delia Drel <d.drel@hotmail.com>
2021-02-04 20:51:27 +0100

    done

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+// //Iteration 2 - Create a recipe
+let someRecipe = {
+  title: "Rigatoni alla Genovese",
+  level: "Easy Peasy",
+  ingredients: [
+    "2 pounds red onions, sliced salt to taste",
+    "2 (16 ounce) boxes uncooked rigatoni",
+    "1 tablespoon chopped fresh marjoram leaves",
+    "1 pinch cayenne pepper",
+    "2 tablespoons freshly grated Parmigiano-Reggiano cheese"
+  ],
+  cuisine: "Italian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg",
+  duration: 220,
+  creator: "Chef Luigi"
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,8 +39,53 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+// Iteration 2 - Create a recipe
+let createPromise = Recipe.create(someRecipe)
+   .then((result) => {
+     console.log("title", result)
+   })
+   .catch((err) => {
+       console.log('Something went wrong while inserting')
+    })
+
+// Iteration 3 - Insert multiple recipes
+let insertPromise = Recipe.insertMany(data)
+  .then(() => {
+      console.log('multiple recipes created')
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .catch(() => {
+      console.log('Something went wrong while inserting')
+  })
+
+// // Iteration 4 - Update recipe
+  Promise.all([createPromise, insertPromise]) 
+    .then(() => {
+      Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then((result) => {
+        console.log('updated', result)
+      })
+  })
+  .catch(() => {
+      console.log('Something went wrong while inserting')
+  })
+
+  // Iteration 5 - Remove a recipe
+  // .then(() => {
+  //   Recipe.deleteOne({ title: 'Carrot Cake' })
+  //     .then((result) => {
+  //       console.log('deleting recipe', result)
+  //   })
+  //   .catch(() => {
+  //     console.log('Something went wrong while inserting')
+})
+
+// Iteration 6 - Close the Database
+mongoose.connection.close()
+
+ 
+  
+ 
+  
+  
+  
